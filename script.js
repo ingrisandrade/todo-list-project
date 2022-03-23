060bdd2770efc8368b5524c33c7c2fde3475947f
Ingris Andrade <iac22@outlook.com>
2022-03-23 17:02:03 -0300

    Altera cor de fundo do item para cinza ao clicar

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const button = document.querySelector('#criar-tarefa');
 const orderList = document.querySelector('#lista-tarefas');
 
+// Ao clicar no botão um novo item é criado ao final da lista e o texto do input é removido.
 function criaElemento() {
   const criaLi = document.createElement('li');
   const textoInput = document.querySelector('#texto-tarefa');
@@ -14,3 +15,13 @@ function criaElemento() {
 }
 criaElemento();
 button.addEventListener('click', criaElemento);
+
+// Ao clicar em um item da lista, altera a cor de fundo do item para cinza.
+const selecionaLi = document.querySelector('#lista-tarefas');
+
+function adicionaGrey(event) {
+  event.target.style.backgroundColor = 'gray';
+}
+selecionaLi.addEventListener('click', adicionaGrey);
+
+
